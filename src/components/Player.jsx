f28f58aa7468fa5b57695a489c82efbf1c0af6c8
Yahya Al-hamedi <y.hamedi@jisr.net>
2025-05-27 09:51:29 +0300

    Cahnge header name using state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,28 @@
+import { useState } from 'react'
+
 export default function Player() {
+  const [playerName, setPlayerName] = useState('')
+  const [isNamed, setIsNamed] = useState(false)
+
+  const handleNameChange = (event) => {
+    setIsNamed(false)
+    setPlayerName(event.target.value)
+  }
+
+  const handleSetName = () => {
+    setIsNamed(true)
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {isNamed ? playerName : 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          type="text"
+          onChange={handleNameChange}
+          value={playerName}
+        />
+        <button onClick={handleSetName}>Set Name</button>
       </p>
     </section>
   )
