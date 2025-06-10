936c07cddb5a1e6485fa8d751884298560000daf
Yahya Al-hamedi <y.hamedi@jisr.net>
2025-06-10 14:34:27 +0300

    Build TimerChallenge component

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,16 @@
+function TimerChallenge({ title, targetTime }) {
+  return (
+    <section className="challenge">
+      <h2>{title}</h2>
+      <p className="challenge-time">
+        {targetTime} second{targetTime > 1 ? 's' : ''}
+      </p>
+      <p>
+        <button>Start Challenge</button>
+      </p>
+      <p>Time is running... / Time is inactive</p>
+    </section>
+  )
+}
+
+export default TimerChallenge
