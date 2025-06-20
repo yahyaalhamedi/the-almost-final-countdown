19c2cfe81f140de19363787c644b061e770e521b
Yahya Al-hamedi <y.hamedi@jisr.net>
2025-06-20 19:42:26 +0300

    use useImperativeHandle React hook

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,4 @@
-import { forwardRef } from 'react'
+import { forwardRef, useImperativeHandle, useRef } from 'react'
 
 // forwardRef is a React utility that allows you to pass refs down to child components.
 // it optional in react 19.0 and later, but it is a good practice to use it when you need to access the DOM element directly.
@@ -9,9 +9,21 @@ import { forwardRef } from 'react'
 // In this case, we use it to show the dialog when the timer expires.
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+  const dialog = useRef()
+
+  // useImperativeHandle is a React hook that allows you to customize the instance value that is exposed to parent components when using refs.
+  // In this case, we expose an `open` method that shows the dialog when called.
+  // This allows parent components to control the dialog's visibility without directly manipulating the DOM.
+  // This is useful for encapsulating the logic of showing the dialog and keeping the component reusable.
+  useImperativeHandle(ref, () => ({
+    open: () => {
+      dialog.current.showModal()
+    },
+  }))
+
   return (
     <dialog
-      ref={ref}
+      ref={dialog}
       className="result-modal"
     >
       <h2>You {result}</h2>
--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -10,7 +10,7 @@ function TimerChallenge({ title, targetTime }) {
   const handleStart = () => {
     timerId.current = setTimeout(() => {
       setTimerExpired(true)
-      dialog.current.showModal()
+      dialog.current.open()
     }, targetTime * 1000)
     setTimerStarted(true)
   }
