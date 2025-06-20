373c3e55f8361facaa5d52598886d416c4614717
Yahya Al-hamedi <y.hamedi@jisr.net>
2025-06-20 17:52:13 +0300

    Use useRef hook to handle start and stop timers

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,14 +1,35 @@
+import { useRef, useState } from 'react'
 function TimerChallenge({ title, targetTime }) {
+  const timerId = useRef()
+
+  const [timerStarted, setTimerStarted] = useState(false)
+  const [timerExpired, setTimerExpired] = useState(false)
+
+  const handleStart = () => {
+    timerId.current = setTimeout(() => {
+      setTimerExpired(true)
+    }, targetTime * 1000)
+    setTimerStarted(true)
+  }
+  const handleStop = () => {
+    clearTimeout(timerId.current)
+  }
+
   return (
     <section className="challenge">
       <h2>{title}</h2>
+      {timerExpired && <p>You lost!</p>}
       <p className="challenge-time">
         {targetTime} second{targetTime > 1 ? 's' : ''}
       </p>
       <p>
-        <button>Start Challenge</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+          {timerStarted ? 'Stop' : 'Start'} Challenge
+        </button>
       </p>
-      <p>Time is running... / Time is inactive</p>
+      <p className={timerStarted ? 'active' : undefined}>
+        {timerStarted ? 'Time is running...' : 'Time is inactive'}
+      </p>
     </section>
   )
 }
